7d6315086756229d7a2b7348d641f97e3e4a7f22
Erol Mrzic <erol.mrzic@gmail.com>
2018-03-15 18:27:17 +0100

    kviz radi, pogadjanje brojeva

--- veci manji broj, kviz, igra.py
+++ veci manji broj, kviz, igra.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 x=int(input("My secret number is: " ))
 low=0
 high=100
@@ -23,29 +22,7 @@ Enter 'c' to indicate I guessed correctly. "))
         break
     else:
         print("Sorry, I did not understand your input.")
-=======
-x=int(input("My secret number is: " ))
-low=0
-high=100
-ans=int((low+high)/2)
-print ("Think of a secret number!" )
+    ans = int((low + high) / 2)
 
-yans=str(input)
 
-while yans!="c":
-    print("Is your number: " + str(ans) + " ?")
 
-    yans = str(input("Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low.\
-Enter 'c' to indicate I guessed correctly. "))
-    if yans == "l":
-        low=ans
-
-    elif yans=="h":
-        high=ans
-
-    elif yans == "c":
-        print("Game over. Your secret number was:" + str(ans))
-        break
-    else:
-        print("Sorry, I did not understand your input.")
->>>
