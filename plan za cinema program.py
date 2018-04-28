6c80fc242d0cd444cae49bf2f41fa1b73511814f
erol713 <36904120+erol713@users.noreply.github.com>
2018-04-29 01:54:41 +0200

    Delete plan za cinema program.py

--- plan za cinema program.py
+++
@@ -1,8 +0,0 @@
-- izlistati sve foldere sa filmovima unutar cineme URADJENO
-- izlistati sve foldere unutar filmova URADJENO
-- razlikovati srt od mkv i avi i mp4 formata URADJENO
--ako ima 2 srt.srt, ukloniti jedan srt URADJENO
-- kopirati isti naziv i dodati odgovarajuce nastavke URADJENO
-- Aako je subs prije filma, dobija naziv onog filma prije njega - POPRAVITI
-- ako ima folder subs sa vise titloova, sve ih nazvati po filmu - POPRAVITI
-- rijesiti se tackica - POPRAVITI
